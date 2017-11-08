0d2b294a3ee7779732636931da2515d6e669a2bf
Johnfg10 <johnfg2610@gmail.com>
2017-11-08 20:36:49 +0000

    finished basics for index & contact us

--- contactus.php
+++ contactus.php
@@ -10,6 +10,47 @@ include_once 'helpers/html_helpers.php';
 </head>
 <body>
 <?php echo html_helpers::get_navbar() ?>
+
+<br>
+
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <h1>Contact us</h1>
+    </div>
+    <br>
+    <div class="row justify-content-center">
+        <p>
+            some text <br> goes <br> here
+        </p>
+    </div>
+    <hr>
+    <div class="row justify-content-center">
+        <div class="col-lg-4 col-md-4 col-sm-6">
+            <div class="media">
+                <div class="media-middle"> <a href="https://github.com/Civ-Dev"> <img height=100 width="100" class="media-object" src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png" alt="..."> </a> </div>
+                <div class="media-body">
+                    <h2 align="center" class="media-heading">Github</h2>
+                    Github is where the main developing of the game happens. You can post issues and even improvements through Github. </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-6">
+            <div class="media">
+                <div class="media-middle"> <a href="https://discord.gg/hunbYNV"> <img height=100 width="100" class="media-object" src="https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png" alt="..."> </a> </div>
+                <div class="media-body">
+                    <h2 align="center" class="media-heading">Discord</h2>
+                    Discord is what we use to communicate and talk to players and where players can recieve updates and talk to staff. </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-6">
+            <div class="media">
+                <div class="media-middle"> <a href="https://minecraft.curseforge.com/projects/civ2"> <img height="120" width="140" class="media-object" src="https://media-corp.cursecdn.com/attachments/0/204/logo3.png" alt="..."></a></div>
+                <div class="media-body">
+                    <h2 align="center" class="media-heading">Curse</h2>
+                    Curse is where you can access the game files and download the game and updates. The game is also available through the curse app. </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php echo html_helpers::get_scripts() ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -7,9 +7,88 @@ include_once 'helpers/html_helpers.php';
     <meta charset="UTF-8">
     <title>Civilisations 2: The Rebirth</title>
     <?php echo html_helpers::get_styles() ?>
+
+    <style>
+        .thumbnail {
+            position: relative;
+        }
+
+        .caption {
+            position: absolute;
+            top: 45%;
+            left: 0;
+            width: 100%;
+        }
+
+        .size {
+            height: 600px;
+            width: 100%;
+        }
+    </style>
 </head>
 <body>
 <?php echo html_helpers::get_navbar() ?>
+<div id="carouselExampleControls" class="carousel slide size" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item size active">
+            <div class="thumbnail text-center">
+                <img class="d-block size" src="images/background.jpg" alt="First slide">
+                <div class="caption" style="color: #ffffff;">
+                    <h1>Civilisations 2: The Rebirth</h1>
+                    <p>A Modded Minecraft Modpack</p>
+                </div>
+            </div>
+
+        </div>
+        <div class="carousel-item size">
+            <div class="thumbnail text-center">
+                <img class="d-block size" src="images/background.jpg" alt="First slide">
+                <div class="caption" style="color: #ffffff;">
+                    <h1>Civilisations 2: The Rebirth</h1>
+                    <p>A Modded Minecraft Modpack</p>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item size">
+            <div class="thumbnail text-center">
+                <img class="d-block size" src="images/background.jpg" alt="First slide">
+                <div class="caption" style="color: #ffffff;">
+                    <h1>Civilisations 2: The Rebirth</h1>
+                    <p>A Modded Minecraft Modpack</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+<div class="container-fluid justify-content-center">
+    <div class="row">
+        <hr>
+    </div>
+    <div class="row justify-content-center">
+        <h1>The project</h1>
+    </div>
+    <div class="row">
+        <hr>
+    </div>
+    <div class="row justify-content-center">
+        <p>Civilisations 2: The Rebirth is a Modded Minecraft Modpack which is made for experienced players. It has hundreds of mods and is filled with challenging custom recipes to make the pack unique and fun. The many mods allows the pack to be enjoyable for all players.</p>
+    </div>
+    <div class="row justify-content-center">
+        <p>The pack is currently in Alpha stage and is always being updated and improved. We are always looking for test gamers as we develop and make the pack better. Your feedback is always helpful and appreciated. For any feedback please post in the Issues section on Github.
+            Feel free to join our Discord server for any queries or questions about the pack.</p>
+    </div>
+</div>
+<hr>
+
+<?php echo html_helpers::get_footer() ?>
 <?php echo html_helpers::get_scripts() ?>
 </body>
 </html>
