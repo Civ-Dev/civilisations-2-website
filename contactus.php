15ce471ee674316ccd89e8a674d1f0813f5d685e
Johnfg10 <johnfg2610@gmail.com>
2017-11-09 11:21:51 +0000

    improved contactus layout closes #1

--- contactus.php
+++ contactus.php
@@ -25,6 +25,21 @@ include_once 'helpers/html_helpers.php';
     </div>
     <hr>
     <div class="row justify-content-center">
+        <div class="col mx-auto text-center">
+            <a href="https://github.com/Civ-Dev"> <img height=100 width="100" src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png" alt="..."> </a>
+            <p>Github is where the main developing of the game happens. You can post issues and even improvements through Github.</p>
+        </div>
+        <div class="col mx-auto text-center">
+            <a href="https://discord.gg/hunbYNV"> <img height=100 width="100" src="https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png" alt="..."> </a>
+            <p>Discord is what we use to communicate and talk to players and where players can recieve updates and talk to staff.</p>
+        </div>
+        <div class="col mx-auto text-center">
+            <a href="https://github.com/Civ-Dev"> <img height=100 width="150" src="https://media-corp.cursecdn.com/attachments/0/204/logo3.png" alt="..."> </a>
+            <p>Curse is where you can access the game files and download the game and updates. The game is also available through the curse app.</p>
+        </div>
+    </div>
+    <!--
+    <div class="row justify-content-center">
         <div class="col-lg-4 col-md-4 col-sm-6">
             <div class="media">
                 <div class="media-middle"> <a href="https://github.com/Civ-Dev"> <img height=100 width="100" class="media-object" src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png" alt="..."> </a> </div>
@@ -50,6 +65,7 @@ include_once 'helpers/html_helpers.php';
             </div>
         </div>
     </div>
+    -->
 </div>
 <?php echo html_helpers::get_scripts() ?>
 </body>
